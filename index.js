22037fd853eb92686a7e61a46d98817f42a5e958
mwaz <waweru.mwaura@allangray.co.za>
2019-03-11 20:27:12 +0300

    none: initialize application

---
+++ index.js
@@ -0,0 +1,52 @@
+import dBConfig from './utils/dbConfig';
+import passport from 'passport';
+import path from 'path';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import routes from './app/routes';
+import  bodyParser from 'body-parser';
+import express from 'express';
+import jsend from 'jsend'
+
+
+mongoose
+  .connect(
+    dBConfig.database,
+    { useNewUrlParser: true}
+  )
+  .then(() => {
+    console.log('successfully connected to the database');
+  })
+  .catch(() => {
+    console.log('unable to connect to the database  Exiting now..');
+    process.exit();
+  });
+  mongoose.set('useCreateIndex', true);
+
+const app = express();
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(jsend.middleware);
+
+// routes(app);
+app.use('/sms-mgt/api', routes)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./utils/auth/passport')(passport);
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+const port = dBConfig.port || 3005;
+
+const server = app.listen(port, function() {
+  console.log('app running on', server.address().port);
+});
+module.exports = server;
